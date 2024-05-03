e30e2b07e51a4f9c96ac6c335cf11237b7bfb1bb
AlexandrStatkevich <96616991+AlexandrStatkevich@users.noreply.github.com>
2024-05-03 06:57:51 +0300

    Update FirstGit.java
    Add symbol ! to sout FirstGit

--- src/main/java/by/alst/FirstGit.java
+++ src/main/java/by/alst/FirstGit.java
@@ -3,6 +3,6 @@ package by.alst;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
-        System.out.println("Learning");
+        System.out.println("Learning!");
     }
 }
