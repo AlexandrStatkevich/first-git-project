7b8ebb1e70871488018bdeb0124a77010d9ab195
AlexandrStatkevich <cof_in_cob@rambler.ru>
2024-05-04 19:56:48 +0300

    change sout

--- src/main/java/by/alst/FirstGit.java
+++ src/main/java/by/alst/FirstGit.java
@@ -4,7 +4,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("Learning!");
-        System.out.println("It's terribly difficult");
+        System.out.println("It's terribly difficult hvjvjv");
         System.out.println("I'm happy");
         System.out.println("!!!!");
     }
