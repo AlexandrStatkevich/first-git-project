a10944eb93067d7ee1b7190a43139243ae018c86
AlexandrStatkevich <96616991+AlexandrStatkevich@users.noreply.github.com>
2024-05-04 19:13:37 +0300

    Update FirstGit.java
    add sout

--- src/main/java/by/alst/FirstGit.java
+++ src/main/java/by/alst/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("Learning!");
+        System.out.println("I'm happy");
     }
 }
