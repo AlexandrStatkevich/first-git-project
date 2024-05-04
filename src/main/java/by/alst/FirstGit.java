4357d328f64c22d5b8b7e97e270b5bd819c27d23
AlexandrStatkevich <cof_in_cob@rambler.ru>
2024-05-04 19:38:54 +0300

    add sout

--- src/main/java/by/alst/FirstGit.java
+++ src/main/java/by/alst/FirstGit.java
@@ -6,5 +6,6 @@ public class FirstGit {
         System.out.println("Learning!");
         System.out.println("It's terribly difficult");
         System.out.println("I'm happy");
+        System.out.println("!!!!");
     }
 }
